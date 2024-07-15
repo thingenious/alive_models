488b213831d11dc75390da6f1a8f8f46dbb9dda6
Lazaros Toumanidis <laztoum@protonmail.com>
2024-07-15 13:16:40 +0300

    update .env.example, config

--- app/config.py
+++ app/config.py
@@ -22,7 +22,7 @@ _have_cuda = torch.cuda.is_available()
 DEVICE = "cuda" if _have_cuda else "cpu"
 COMPUTE_TYPE = "float16" if _have_cuda else "float32"
 TORCH_DTYPE = torch.float16 if _have_cuda else torch.float32
-USE_FLASH_ATTENTION = _have_cuda
+USE_FLASH_ATTENTION = os.environ.get("USE_FLASH_ATTENTION", str(_have_cuda)).lower() != "false"
 
 _ALL_MODELS = ["asr", "fer", "ser", "nlp", "tts", "lid"]
 
